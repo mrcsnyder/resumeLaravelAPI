8c4c71ba4684de16cc6de48d76dd166d69cc3f97
chrissnyder <snyder.chris.m@gmail.com>
2020-10-04 20:16:54 -0700

    Split Awards index into its own view file to incldue in education index

--- resources/views/education/education-index.blade.php
+++ resources/views/education/education-index.blade.php
@@ -25,23 +25,6 @@
     </div>
 
     <hr/>
-    <h2><i class="fas fa-award"></i> Academic Awards <a class="btn btn-sm btn-dark" href="/education/award/create-award">Create Award</a></h2>
-
-    <div class="row">
-    @foreach($awards as $award)
-
-
-            <div class="col-md-6 col-lg-6 mt-3 text-center">
-                <div class="card" style="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$award->award_name}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{$award->date_range}}</h6>
-                        <a class="btn btn-sm btn-dark" href="/education/award/edit-award/{{$award->id}}">Edit</a>
-                    </div>
-                </div>
-            </div>
-
-    @endforeach
-    </div>
-
+    @include('education.award.index-awards')
+    
 @endsection
